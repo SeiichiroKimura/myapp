1c6d58825d95a450ec00f14914cb12d627ee2094
SeiichiroKimura <skim.lalala@gmail.com>
2020-01-18 17:51:03 +0900

    add detail html

--- bbs/models.py
+++ bbs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    content = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=200, null = True)
+
+    def __str__(self):
+        return self.content
--- bbs/views.py
+++ bbs/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Article
 
 def index(request):
+    articles = Article.objects.all()
     context = {
-        'message': 'welcome to bbs app',
-        'players': ['勇者', '魔法使い', '剣士', 'サル'],
-        'enemy': 'スライム'
-        }
+        'message': 'Welcome my BBS',
+        'articles': articles,
+    }
     return render(request, 'bbs/index.html', context)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message': 'Show Article ' + str(id),
+        'article': article,
+    }
+    return render(request, 'bbs/detail.html', context)
